5f7ef2ce895c225373acdfb57249fa66a75a485b
santiago9513 <santiagoo9513@gmail.com>
2025-05-28 16:05:37 -0300

    agrega tabla de salones y guardado con localStorage

--- js/altaSalon.js
+++ js/altaSalon.js
@@ -22,8 +22,38 @@ form.addEventListener('submit', function(event){
     const valor = document.getElementById('valor').value; 
     const descripcion = document.getElementById('descripcion').value;
 
+    const salon = {nombre, valor, descripcion};
+    const salones = JSON.parse(localStorage.getItem('salones')) || [];  //carga salones o inicializa vacio
+    salones.push(salon);    //agrega el salon al arreglo de salones
+    localStorage.setItem('salones', JSON.stringify(salones))    //guarda los salones
+
     alert(`El nuevo salon tiene los siguientes atributos Nombre: ${nombre}.Precio: ${valor}.Descripcion: ${descripcion}`);
 
-    this.reset();
+    this.reset();   //limpia el formulario despues de enviarlo
+
+    mostrarSalones();
+
     });
+
+mostrarSalones()
 });
+
+function mostrarSalones(){
+    
+    const tablaBody = document.querySelector('#tablaSalones tbody');    
+    
+    tablaBody.innerHTML = ''; 
+    
+    const salones = JSON.parse(localStorage.getItem('salones')) || [];
+    
+    salones.forEach((salon) => {
+        const fila = document.createElement('tr');
+        fila.innerHTML = `
+        <td>${salon.nombre}</td>
+        <td>${salon.valor}</td>
+        <td>${salon.descripcion}</td>
+        `;
+
+        tablaBody.appendChild(fila);
+    })
+}
