9fd264ab1a73edbdbe0769be4de009ec334bcc44
santiago9513 <santiagoo9513@gmail.com>
2025-06-04 19:26:34 -0300

    Agrega valores faltantes para inicializacion de salones

--- js/catalogoSalones.js
+++ js/catalogoSalones.js
@@ -3,7 +3,7 @@
 const salonesIniciales = [
   {
     nombre: "Salón Aire Libre",
-    direccion: "San Martin 1500",
+    direccion: "San Martin 456",
     descripcion: "Un espacio fresco y natural, ideal para eventos al aire libre, con juegos interactivos y áreas verdes para disfrutar del aire libre en cualquier ocasión.",
     valor: 35000,
     estado: "Reservado",
@@ -11,32 +11,42 @@ const salonesIniciales = [
   },
   {
     nombre: "Salón Aventura",
+    direccion: "Av. Rivadavia 1234",
     descripcion: "Un salón lleno de emociones y diversión, con juegos para escalar y moverse. Perfecto para niños inquietos.",
     valor: 40000,
+    estado: "Disponible",
     imagen: "img/ImagenSalonAventura.jpg"
   },
   {
     nombre: "Salón Cuento",
+    direccion: "Mitre 1010",
     descripcion: "Inspirado en cuentos, con rincón de lectura y decoración mágica.",
     valor: 38000,
+    estado: "Disponible",
     imagen: "img/ImagenSalonCuento.jpg"
   },
   {
     nombre: "Salón Selva",
+    direccion: "Av. Colón 789",
     descripcion: "Un ambiente exótico inspirado en la naturaleza, te invita a jugar y divertirte.",
     valor: 37000,
+    estado: "Disponible",
     imagen: "img/ImagenSalonSelva.jpg"
   },
   {
     nombre: "Salón FarmHouse",
+    direccion: "Quintana 6670",
     descripcion: "Ofrece un entorno cálido, natural y lleno de encanto. La protagonista: una gran casita estilo granja, rodeada de aire libre y decoración campestre, donde los peques pueden jugar, explorar y conectarse con lo simple y lo lindo de la infancia.",
     valor: 60000,
+    estado: "Reservado",
     imagen: "img/ImagenSalonFarmHouse.jpg"
   },
   {
     nombre: "Salón Baby",
+    direccion: "Magallanes 620",
     descripcion: "Diseñado especialmente para bebés de 0 a 2 años, con espacios seguros, suaves y estimulantes para que descubran el mundo a su ritmo. Juegos sensoriales, colores suaves, rincones de exploración y mucho amor en cada detalle.",
     valor: 50000,
+    estado: "Disponible",
     imagen: "img/ImagenSalonBaby.jpg"
   }
 ];
